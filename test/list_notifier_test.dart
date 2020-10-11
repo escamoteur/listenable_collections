17a63e445c4cd4faad6d466d318664c05c366de5
dancamdev <dancam.dev@gmail.com>
2020-10-11 12:25:07 +0200

    updated tests

--- test/list_notifier_test.dart
+++ test/list_notifier_test.dart
@@ -1,157 +1,214 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import '../lib/listenable_collections.dart';
+import '../lib/src/list_notifier.dart';
 
 void main() {
-  ListNotifier list;
+  group("Tests for the ListNotifier's methods", () {
+    ListNotifier list;
+    List result = [];
 
-  setUp(() {
-    list = ListNotifier(data: [1, 2, 3]);
-  });
-
-
-  test("Elements get swapped correctly", () {
-    list.swap(0, 2);
-
-    expect(list.value, [3, 2, 1]);
-  });
-
-  test("Listeners get updated if a value gets added", () {
-    list.addListener(() {
-      expect(list[3], 4);
+    setUp(() {
+      list = ListNotifier(data: [1, 2, 3]);
+      result.clear();
     });
 
-    list.add(4);
-  });
+    void buildListener() {
+      list.addListener(() {
+        result.addAll(list.value);
+      });
+    }
 
-  test("Listeners get notified if an iterable is added", () {
-    list.addListener(() {
-      expect(list.value, [1,2,3,4,5]);
+    test("Elements get swapped correctly", () {
+      buildListener();
+
+      list.swap(0, 2);
+
+      expect(result, [3, 2, 1]);
     });
 
-    list.addAll([4, 5]);
-  });
+    test("Listeners get updated if a value gets added", () {
+      buildListener();
 
-  test("Listeners get notified if the list is cleared", () {
-    list.addListener(() {
-      expect(list.value, []);
-    });
-    
-    list.clear();
-  });
+      list.add(4);
 
-  test("Listeners get notified on fillRange", () {
-    list.addListener(() {
-      expect(list.value, [1, 1, 1]);
+      expect(result[3], 4);
     });
 
-    list.fillRange(0, list.length, 1);
-  });
+    test("Listeners get notified if an iterable is added", () {
+      buildListener();
 
-  test("Listeners get notified on value insertion", () {
-    list.addListener(() {
-      expect(list.value, [1, 1, 3]);
+      list.addAll([4, 5]);
+
+      expect(result, [1, 2, 3, 4, 5]);
     });
 
-    list.insert(1, 1);
-  });
+    test("Listeners get notified if the list is cleared", () {
+      buildListener();
 
-  test("Listeners get notified on iterable insertion", () {
-    list.addListener(() {
-      expect(list.value, [1, 1, 2, 3]);
+      list.clear();
+
+      expect(result, []);
     });
 
-    list.insertAll(1, [1, 2]);
-  });
+    test("Listeners get notified on fillRange", () {
+      buildListener();
 
-  test("Listeners get notified on value removal", () {
-    list.addListener(() {
-      expect(list.value, [1, 3]);
+      list.fillRange(0, list.length, 1);
+
+      expect(result, [1, 1, 1]);
     });
 
-    final result = list.remove(2);
-    expect(result, true);
-  });
+    test("Listeners get notified on value insertion", () {
+      buildListener();
 
-  test("Listeners get notified on index removal", () {
-    list.addListener(() {
-      expect(list.value, [1, 3]);
+      list.insert(1, 1);
+
+      expect(result, [1, 1, 2, 3]);
     });
 
-    final itemRemoved = list.removeAt(1);
-    expect(itemRemoved, 2);
-  });
+    test("Listeners get notified on iterable insertion", () {
+      buildListener();
 
-  test("Listeners get notified on last element removal", () {
-    list.addListener(() {
-      expect(list.value, [1, 2]);
+      list.insertAll(1, [1, 2]);
+
+      expect(result, [1, 1, 2, 2, 3]);
     });
 
-    final itemRemoved = list.removeLast();
-    expect(itemRemoved, 3);
-  });
+    test("Listeners get notified on value removal", () {
+      buildListener();
 
-  test("Listeners get notified on range removal", () {
-    list.addListener(() {
-      expect(list.value, [3]);
+      final itemIsRemoved = list.remove(2);
+
+      expect(result, [1, 3]);
+      expect(itemIsRemoved, true);
     });
 
-    list.removeRange(0, 2);
-  });
+    test("Listeners get notified on index removal", () {
+      buildListener();
 
-  test("Listeners get notified on removeWhere", () {
-    list.addListener(() {
+      final removedItem = list.removeAt(1);
+
+      expect(result, [1, 3]);
+      expect(removedItem, 2);
+    });
+
+    test("Listeners get notified on last element removal", () {
+      buildListener();
+
+      final itemRemoved = list.removeLast();
+
+      expect(result, [1, 2]);
+      expect(itemRemoved, 3);
+    });
+
+    test("Listeners get notified on range removal", () {
+      buildListener();
+
+      list.removeRange(0, 2);
+
+      expect(result, [3]);
+    });
+
+    test("Listeners get notified on removeWhere", () {
+      buildListener();
+
+      list.removeWhere((element) => element == 1);
+
       expect(list.value, [2, 3]);
     });
 
-    list.removeWhere((element) => element == 1);
-  });
+    test("Listeners get notified on replaceRange", () {
+      buildListener();
 
-  test("Listeners get notified on replaceRange", () {
-    list.addListener(() {
-      expect(list.value, [3, 3, 3]);
+      list.replaceRange(0, 2, [3, 3]);
+
+      expect(result, [3, 3, 3]);
     });
 
-    list.replaceRange(0, 2, [3]);
-  });
+    test("Listeners get notified on retainWhere", () {
+      buildListener();
 
-  test("Listeners get notified on retainWhere", () {
-    list.addListener(() {
+      list.retainWhere((element) => element == 1);
+
       expect(list.value, [1]);
     });
 
-    list.retainWhere((element) => element == 1);
-  });
+    test("Listeners get notified on setAll", () {
+      buildListener();
 
-  test("Listeners get notified on setAll", () {
-    list.addListener(() {
+      list.setAll(2, [2]);
+
       expect(list.value, [1, 2, 2]);
     });
 
-    list.setAll(2, [2]);
-  });
+    test("Listeners get notified on setRange", () {
+      buildListener();
 
-  test("Listeners get notified on setRange", () {
-    list.addListener(() {
-      expect(list.value, [1, 2, 2]);
+      list.setRange(2, list.length, [2]);
+
+      expect(result, [1, 2, 2]);
     });
 
-    list.setRange(2, list.length, [2]);
-  });
+    test("Listeners get notified on shuffle", () {
+      buildListener();
 
-  test("Listeners get notified on shuffle", () {
-    list.addListener(() {
-      expect(list.value != [1,2,3], true);
+      list.shuffle();
+
+      expect(result != [1, 2, 3], true);
     });
 
-    list.shuffle();
+    test("Listeners get notified on sort", () {
+      buildListener();
+
+      list.sort((value1, value2) => -(value1.compareTo(value2)));
+
+      expect(result, [3, 2, 1]);
+    });
   });
 
-  test("Listeners get notified on sort", () {
-    list.addListener(() {
-      expect(list.value, [3,2,1]);
+  group("Tests for the ListNotifier's equality", () {
+    List result;
+
+    setUp(() {
+      result = null;
     });
 
-    list.sort((value1, value2) => - (value1.compareTo(value2)));
+    test("The listener isn't notified if the value is equal", () {
+      final ListNotifier list = ListNotifier(
+        data: [1, 2, 3],
+        dontNotifyIfEqual: true,
+      );
+
+      list.addListener(() {
+        result = [...list.value];
+      });
+
+      list[0] = 1;
+
+      expect(result, null);
+    });
+
+    test("customEuqality works correctly", () {
+      final ListNotifier list = ListNotifier(
+        data: [1, 2, 3],
+        dontNotifyIfEqual: true,
+        customEquality: (index, value) => value >= 3,
+      );
+
+      list.addListener(() {
+        result = [...list.value];
+      });
+
+      list[2] = 3;
+
+      expect(result, null);
+
+      list[0] = 1;
+
+      // if customEquality wasn't implemented this would not call
+      // the listeners, it doea because 1 < 3, as defined in
+      // custom equality.
+      expect(result, [1, 2, 3]);
+    });
   });
 }
