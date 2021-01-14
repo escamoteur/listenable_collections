3382cd48dc6673a9ead7e66c4a2fe14df1fe5681
Mboya Michael <42235564+mboyamike@users.noreply.github.com>
2021-01-14 07:16:46 +0300

    Adding tests
    Added a few tests for the map notifier

--- test/map_notifier_test.dart
+++ test/map_notifier_test.dart
@@ -16,6 +16,7 @@ void main() {
     void buildListener() {
       mapNotifier.addListener(() {
         result.addAll(mapNotifier.value);
+        print(result);
       });
     }
 
@@ -28,13 +29,25 @@ void main() {
     test('Listeners get updated if a key value pair is removed', () {
       buildListener();
       mapNotifier.remove('two');
-      expect(mapNotifier.length, 1);
+      expect(result.length, 1);
     });
 
     test('Listeners get updated if MapNotifier is cleared', (){
       buildListener();
       mapNotifier.clear();
       expect(result, {});
+    });
+
+    test('Listeners get updated after adding multiple entries', () {
+      buildListener();
+      mapNotifier.addEntries({'three': 3, 'four': 4}.entries);
+      expect(result.length, 4);
+    });
+
+    test('Listeners get updated when putIfAbsent is called', () {
+      buildListener();
+      mapNotifier.putIfAbsent('five', () => 5);
+      expect(result['five'], 5);
     });
   });
 }
