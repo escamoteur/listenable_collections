a3ad4c7deec44841341df06afe27202c4bc8613e
Mboya Michael <mikemboya2@gmail.com>
2022-03-30 08:00:13 +0300

    map notifier full tests and upgrade to null safety

--- lib/src/map_notifier.dart
+++ lib/src/map_notifier.dart
@@ -1,116 +1,135 @@
 import 'package:collection/collection.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 
-/// A map that behaves like [ValueNotifier] if its data changes
-/// It does not compare the elements on bulk operations
-
-class MapNotifier<K, V> extends DelegatingMap<K, V> with ChangeNotifier 
+class MapNotifier<K, V> extends DelegatingMap<K, V>
+    with ChangeNotifier
     implements ValueListenable<Map<K, V>> {
+  MapNotifier({
+    Map<K, V>? data,
+    bool notifyIfEqual = false,
+  })  : _notifyIfEqual = notifyIfEqual,
+        super(data ?? {});
 
-  /// Creates a new listenable map.
-  /// [data] Optional Map that can be passed as initial value
-  /// if notifyIfEqual is false, MapNotifier will compare if a value
-  /// passed is equal to the existing value
-  /// Like `map['version'] = '2.0.7'` will only notify
-  MapNotifier({Map<K, V> data, bool notifyIfEqual = true}) : super(data ?? {});
+  final bool _notifyIfEqual;
 
-  void _notify() {
-    super.notifyListeners();
-  }
-
-  /// if this is `true` no listener will be notified if the map changes.
-  bool _inTransaction = false;
-
-  /// Starts a transaction that allows to make multiple changes to the Map
-  /// with only one notification at the end.
-  void startTransAction() {
-    assert(!_inTransaction, 'Only one transaction at a time in MapNotifier');
-    _inTransaction = true;
-  }
-
-  /// Ends a transaction
-  void endTransAction() {
-    assert(_inTransaction, 'No active transaction in MapNotifier');
-    _inTransaction = false;
-    _notify();
-  }
-
-  /// Returns an unmodifiable view on the map's data
   @override
   Map<K, V> get value => UnmodifiableMapView(this);
 
-  /// from here all functions are equal to `List<T>` with the addition that all
-  /// modifying functions will call `notifyListener` if not in a transaction.
-
   @override
-  V operator [](Object key) => super[key];
-
-  @override
-  void operator []=(K key, V value) {
+  operator []=(K key, V value) {
+    final areEqual = super[key] == value;
     super[key] = value;
-    _notify();
+
+    if (!areEqual || (areEqual && _notifyIfEqual)) {
+      notifyListeners();
+    }
   }
 
   @override
   void addAll(Map<K, V> other) {
+    final initialMapValue = {...this};
     super.addAll(other);
-    _notify();
+
+    final notify = _shouldNotify(initialMapValue);
+    if (notify) {
+      notifyListeners();
+    }
+  }
+
+  /// A method to compare between two maps, this Map Notifier and any other
+  /// map that has been passed to it
+  bool _areEqual(Map<K, V> other) {
+    if (this.length != other.length) {
+      return false;
+    }
+
+    for (final key in keys) {
+      if (this[key] != other[key]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  bool _shouldNotify(Map<K, V> other) {
+    return !_areEqual(other) || (_areEqual(other) && _notifyIfEqual);
   }
 
   @override
   void addEntries(Iterable<MapEntry<K, V>> entries) {
+    final initialMapValue = {...this};
     super.addEntries(entries);
-    _notify();
+
+    if (_shouldNotify(initialMapValue)) {
+      notifyListeners();
+    }
   }
 
   @override
   void clear() {
+    final shouldNotify = isNotEmpty || _notifyIfEqual;
     super.clear();
-    _notify();
+
+    if (shouldNotify) {
+      notifyListeners();
+    }
   }
 
   @override
   V putIfAbsent(K key, V Function() ifAbsent) {
-    V v = super.putIfAbsent(key, ifAbsent);
-    _notify();
-    return v;
+    final initialMapValue = {...this};
+    final value = super.putIfAbsent(key, ifAbsent);
+
+    if (_shouldNotify(initialMapValue)) {
+      notifyListeners();
+    }
+    return value;
   }
 
   @override
-  V remove(Object key) {
-    final initialLength = this.value.length;
-    V v = super.remove(key);
+  V? remove(Object? key) {
+    final initialMapValue = {...this};
+    final value = super.remove(key);
 
-    if(this.value.length != initialLength)
-      _notify();
+    if (_shouldNotify(initialMapValue)) {
+      notifyListeners();
+    }
 
-    return v;
+    return value;
   }
 
   @override
   void removeWhere(bool Function(K, V) test) {
-    final initialLength = this.value.length;
+    final initialMapValue = {...this};
+
     super.removeWhere(test);
 
-    /// only notify if an element was removed
-    if(this.value.length == initialLength)
-      _notify();
+    if (_shouldNotify(initialMapValue)) {
+      notifyListeners();
+    }
   }
-  
-  @override
-  Iterable<V> get values => super.values;
 
   @override
-  V update(K key, V Function(V) update, {V Function() ifAbsent}) {
-    V v = super.update(key, update, ifAbsent: ifAbsent);
-    _notify();
-    return v;
+  V update(K key, V Function(V) update, {V Function()? ifAbsent}) {
+    final initialMapValue = {...this};
+    final value = super.update(key, update, ifAbsent: ifAbsent);
+
+    if (_shouldNotify(initialMapValue)) {
+      notifyListeners();
+    }
+
+    return value;
   }
 
   @override
   void updateAll(V Function(K, V) update) {
+    final initialMapValue = {...this};
+
     super.updateAll(update);
-    _notify();
+
+    if (_shouldNotify(initialMapValue)) {
+      notifyListeners();
+    }
   }
 }
--- test/map_notifier_test.dart
+++ test/map_notifier_test.dart
@@ -1,53 +1,255 @@
-import 'dart:io';
-
 import 'package:flutter_test/flutter_test.dart';
 import '../lib/src/map_notifier.dart';
 
 void main() {
-  group('Tests for Map Notifiers methods', () {
-    MapNotifier mapNotifier;
-    Map result = {};
+  group('Tests for MapNotifier methods', () {
+    late MapNotifier<String, int> mapNotifier;
+    Map<String, int> result = {};
+    final newValues = {'zero': 0, 'one': 1, 'two': 2, 'three': 3};
 
     setUp(() {
-      mapNotifier = MapNotifier(data: {'one':'1', 'two': '2'});
-      result.clear();
-    });
-
-    void buildListener() {
+      mapNotifier = MapNotifier(data: {'zero': 0});
       mapNotifier.addListener(() {
-        result.addAll(mapNotifier.value);
-        print(result);
+        result = {...mapNotifier};
       });
-    }
-
-    test('Listeners get updated if a new key value pair is added', () {
-      buildListener();
-      mapNotifier['three'] = '3';
-      expect(result['three'], '3');
     });
 
-    test('Listeners get updated if a key value pair is removed', () {
-      buildListener();
-      mapNotifier.remove('two');
-      expect(result.length, 1);
+    tearDown(() {
+      mapNotifier.dispose();
     });
 
-    test('Listeners get updated if MapNotifier is cleared', (){
-      buildListener();
+    test('Notifies when a new value is added', () {
+      mapNotifier['one'] = 1;
+      expect(result['zero'], 0);
+      expect(result['one'], 1);
+    });
+
+    test('Listener is notified on add all', () {
+      mapNotifier.addAll(newValues);
+      expect(result, newValues);
+    });
+
+    test('Listener is notified on add entries', () {
+      mapNotifier.addEntries(newValues.entries);
+      expect(result, newValues);
+    });
+
+    test('Listener is notified on clear', () {
+      mapNotifier.addAll(newValues);
+      expect(result, newValues);
       mapNotifier.clear();
-      expect(result, {});
+      expect(result.isEmpty, isTrue);
     });
 
-    test('Listeners get updated after adding multiple entries', () {
-      buildListener();
-      mapNotifier.addEntries({'three': 3, 'four': 4}.entries);
-      expect(result.length, 4);
+    test('Listener is notified on putIfAbsent', () {
+      for (final entry in newValues.entries) {
+        mapNotifier.putIfAbsent(entry.key, () => entry.value);
+      }
+      expect(result, newValues);
     });
 
-    test('Listeners get updated when putIfAbsent is called', () {
-      buildListener();
-      mapNotifier.putIfAbsent('five', () => 5);
-      expect(result['five'], 5);
+    test('Listener is notified when a key is removed', () {
+      final key = 'one';
+      mapNotifier[key] = 1;
+      expect(result[key], 1);
+      mapNotifier.remove(key);
+      expect(result[key], isNull);
+    });
+
+    test('Listener is notified when removeWhere is called', () {
+      mapNotifier.addAll(newValues);
+      mapNotifier.removeWhere((_, v) => v > 0);
+      expect(result, {'zero': 0});
+    });
+
+    test('Listener is notified when update is called', () {
+      mapNotifier.update('zero', (_) => 10);
+      expect(result, {'zero': 10});
+    });
+
+    test('Listener is notified when updateAll is called', () {
+      mapNotifier.addAll(newValues);
+      mapNotifier.updateAll((p0, p1) => 1);
+      expect(result, {'zero': 1, 'one': 1, 'two': 1, 'three': 1});
+    });
+  });
+
+  group('Tests for notifyIfEqual', () {
+    late MapNotifier<String, int> mapNotifier;
+    late int listenerCallCount;
+    final newValues = {'zero': 0, 'one': 1};
+
+    group('when notifyIfEqual is false', () {
+      setUp(() {
+        mapNotifier = MapNotifier(notifyIfEqual: false);
+        listenerCallCount = 0;
+        mapNotifier.addListener(() {
+          listenerCallCount++;
+        });
+      });
+
+      tearDown(() {
+        mapNotifier.dispose();
+      });
+
+      test('Listener is not notified if value is equal', () {
+        mapNotifier['zero'] = 0;
+        mapNotifier['zero'] = 0;
+        expect(listenerCallCount, 1);
+      });
+
+      test('Listener is not notified if addAll results in equal value', () {
+        mapNotifier.addAll(newValues);
+        mapNotifier.addAll(newValues);
+
+        expect(listenerCallCount, 1);
+      });
+
+      test('Listener is not notified if addEntries results in equal value', () {
+        mapNotifier.addEntries(newValues.entries);
+        mapNotifier.addEntries(newValues.entries);
+        expect(listenerCallCount, 1);
+      });
+
+      test('Calling clear on an empty map does not notify listeners', () {
+        mapNotifier.clear();
+        expect(listenerCallCount, 0);
+      });
+
+      test('Listener is not notified if the value already existed', () {
+        mapNotifier.putIfAbsent('zero', () => 0);
+        mapNotifier.putIfAbsent('zero', () => 0);
+        mapNotifier.putIfAbsent('zero', () => 1);
+
+        expect(listenerCallCount, 1);
+      });
+
+      test('Listener is not notified if no value is removed', () {
+        mapNotifier.addAll(newValues);
+        expect(listenerCallCount, 1);
+        mapNotifier.remove('zero');
+        expect(listenerCallCount, 2);
+        mapNotifier.remove('zero');
+        expect(listenerCallCount, 2);
+      });
+
+      test(
+        'Listener is not notified if removeWhere does not remove any values',
+        () {
+          mapNotifier.addAll(newValues);
+          expect(listenerCallCount, 1);
+          mapNotifier.removeWhere((key, _) => key == 'ten');
+          expect(listenerCallCount, 1);
+        },
+      );
+
+      test(
+          'Listener is not notified when update is called and updates to the '
+          'already existing value', () {
+        mapNotifier.update('zero', (_) => 10, ifAbsent: () => 10);
+        expect(listenerCallCount, 1);
+        mapNotifier.update('zero', (_) => 10, ifAbsent: () => 10);
+        expect(listenerCallCount, 1);
+      });
+
+      test(
+          'Listener is not notified when updateAll is called and updates to '
+          'already existing value', () {
+        mapNotifier.addAll(newValues);
+        expect(listenerCallCount, 1);
+        mapNotifier.updateAll((p0, p1) => 1);
+        expect(listenerCallCount, 2);
+        mapNotifier.updateAll((p0, p1) => 1);
+        expect(listenerCallCount, 2);
+      });
+    });
+
+    group('when notifyIfEqual is true', () {
+      setUp(() {
+        mapNotifier = MapNotifier(notifyIfEqual: true);
+        listenerCallCount = 0;
+        mapNotifier.addListener(() {
+          listenerCallCount++;
+        });
+      });
+
+      tearDown(() {
+        mapNotifier.dispose();
+      });
+
+      test(
+        'Listener is notified if added value is equal',
+        () {
+          mapNotifier['zero'] = 0;
+          mapNotifier['zero'] = 0;
+          expect(listenerCallCount, 2);
+        },
+      );
+
+      test('Listener is notified on addAll values already exist', () {
+        final newValues = {'zero': 0, 'one': 1};
+        mapNotifier.addAll(newValues);
+        mapNotifier.addAll(newValues);
+
+        expect(listenerCallCount, 2);
+      });
+
+      test('Listener is notified if addEntries results in equal value', () {
+        mapNotifier.addEntries(newValues.entries);
+        mapNotifier.addEntries(newValues.entries);
+        expect(listenerCallCount, 2);
+      });
+
+      test('Calling clear on an empty map notifies listeners', () {
+        mapNotifier.clear();
+        expect(listenerCallCount, 1);
+      });
+
+      test('Listener is notified on putIfAbsent', () {
+        mapNotifier.putIfAbsent('zero', () => 0);
+        expect(listenerCallCount, 1);
+        mapNotifier.putIfAbsent('zero', () => 0);
+        expect(listenerCallCount, 2);
+        mapNotifier.putIfAbsent('zero', () => 1);
+        expect(listenerCallCount, 3);
+      });
+
+      test('Listener is notified if no value is removed', () {
+        mapNotifier.addAll(newValues);
+        expect(listenerCallCount, 1);
+        mapNotifier.remove('zero');
+        expect(listenerCallCount, 2);
+        mapNotifier.remove('zero');
+        expect(listenerCallCount, 3);
+      });
+
+      test(
+        'Listener is notified if removeWhere does not remove any values',
+        () {
+          mapNotifier.addAll(newValues);
+          expect(listenerCallCount, 1);
+          mapNotifier.removeWhere((key, _) => key == 'ten');
+          expect(listenerCallCount, 2);
+        },
+      );
+
+      test('Listener is notified when update is called', () {
+        mapNotifier.update('zero', (_) => 10, ifAbsent: () => 10);
+        expect(listenerCallCount, 1);
+        mapNotifier.update('zero', (_) => 10, ifAbsent: () => 10);
+        expect(listenerCallCount, 2);
+      });
+
+      test(
+          'Listener is notified when updateAll is called and updates to '
+          'already existing value', () {
+        mapNotifier.addAll(newValues);
+        expect(listenerCallCount, 1);
+        mapNotifier.updateAll((p0, p1) => 1);
+        expect(listenerCallCount, 2);
+        mapNotifier.updateAll((p0, p1) => 1);
+        expect(listenerCallCount, 3);
+      });
     });
   });
 }
