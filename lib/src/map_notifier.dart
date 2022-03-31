5d760b35af4acc8a89ef54ae0ec018d177874458
Mboya Michael <mikemboya2@gmail.com>
2022-03-31 10:13:13 +0300

    Add documentation comments

--- lib/src/map_notifier.dart
+++ lib/src/map_notifier.dart
@@ -1,9 +1,33 @@
 import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 
+/// A Map that behaves like `ValueNotifier` if its data changes.
+/// By default, [notifyIfEqual] is set to false. In this state, listeners
+/// are not notified if a value is passed that is equal to the value
+/// currently stored in that key. If set to true, listeners would be notified
+/// of any values that are passed, even when equal.
+/// For example, if notify if equal is false, the code below will not notify
+/// listeners:
+/// '''
+///   final mapNotifier = MapNotifier(data: {'one': 1});
+///   mapNotifier['one'] = 1
+/// '''
 class MapNotifier<K, V> extends DelegatingMap<K, V>
     with ChangeNotifier
     implements ValueListenable<Map<K, V>> {
+  ///
+  /// Creates a Map Notifier. The optional field [data] can be used to specify
+  /// an initial value. It defaults to an empty Map. By default, [notifyIfEqual]
+  /// is set to false. In this state, listeners
+  /// are not notified if a value is passed that is equal to the value
+  /// currently stored in that key. If set to true, listeners would be notified
+  /// of any values that are passed, even when equal.
+  /// For example, if notify if equal is false, the code below will not notify
+  /// listeners:
+  /// '''
+  ///   final mapNotifier = MapNotifier(data: {'one': 1});
+  ///   mapNotifier['one'] = 1
+  /// '''
   MapNotifier({
     Map<K, V>? data,
     bool notifyIfEqual = false,
@@ -11,7 +35,20 @@ class MapNotifier<K, V> extends DelegatingMap<K, V>
   })  : _notifyIfEqual = notifyIfEqual,
         super(data ?? {});
 
+  /// Determines whether to notify listeners if an equal value is passed to
+  /// a key.
+  /// For example, if notify if equal is false, the code below will not notify
+  /// listeners:
+  /// '''
+  ///   final mapNotifier = MapNotifier(data: {'one': 1});
+  ///   mapNotifier['one'] = 1
+  /// '''
   final bool _notifyIfEqual;
+
+  /// [customEquality] can be used to set your own criteria for comparing
+  /// values, which might be important if [notifyIfEqual] is false. The function
+  /// should return a bool that represents if, when compared, two values are
+  /// equal. If null, the default values equality [==] is used.
   final bool Function(V? x, V? y)? customEquality;
 
   @override
